bc4241b13f897cfd18fb9ed64c04f287d32365ed
JiuChiYl <1244769768@qq.com>
2026-04-30 02:17:23 +0800

    uploads

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/JC-CMS-Frontend/',
   plugins: [
     vue(),
     // vueDevTools(),
